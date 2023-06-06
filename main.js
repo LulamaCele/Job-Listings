93694d5bba8c97f7ce8e288a9b911bb2e8ea41f9
Lulama Cele <lulamac32@gmail.com>
2023-06-07 01:16:00 +0200

    Update: I added the keywords/descriptions of the jobs e.g languages, tools, role, and job level. All of these are added dynamically.

--- main.js
+++ main.js
@@ -9,60 +9,96 @@ mainDiv.className = "container";
 
 while (count <= 10) {
   let itemDiv = document.createElement("div");
-  itemDiv.id = "item"; 
-//   + jobData[count].id;
+  itemDiv.id = "item";
+
+  // itemDiv.style.borderImageSource.url = jobData[count].logo;
+  // console.log(itemDiv.style.borderImageSource.url);
+  // mainDiv.appendChild(itemDiv);
+
   let companyLogo = document.createElement("img");
   companyLogo.src = jobData[count].logo;
   itemDiv.appendChild(companyLogo);
 
+  let jobDetails = document.createElement("div");
+  jobDetails.className = "details";
+
   let companyName = document.createElement("p");
   companyName.className = "company";
   companyName.innerHTML = jobData[count].company;
-  itemDiv.appendChild(companyName);
+  jobDetails.appendChild(companyName);
 
   let jobPosition = document.createElement("p");
-jobPosition.className = "position";
-jobPosition.innerHTML = jobData[count].position;
-itemDiv.appendChild(jobPosition);
+  jobPosition.className = "position";
+  jobPosition.innerHTML = jobData[count].position;
+  jobDetails.appendChild(jobPosition);
 
-let ul = document.createElement("ul");
-let daysPosted = document.createElement("li");
-daysPosted.className = "postDate";
-daysPosted.innerHTML = jobData[count].postedAt;
-ul.appendChild(daysPosted);
+  let ul = document.createElement("ul");
+  let daysPosted = document.createElement("li");
+  daysPosted.className = "postDate";
+  daysPosted.innerHTML = jobData[count].postedAt;
+  ul.appendChild(daysPosted);
 
-let contractType = document.createElement("li");
-contractType.className = "contract";
-contractType.innerHTML = jobData[count].contract;
-ul.appendChild(contractType);
+  let contractType = document.createElement("li");
+  contractType.className = "contract";
+  contractType.innerHTML = jobData[count].contract;
+  ul.appendChild(contractType);
 
-let jobLocation = document.createElement("li");
-jobLocation.className = "location";
-jobLocation.innerHTML = jobData[count].location;
-ul.appendChild(jobLocation);
-itemDiv.appendChild(ul);
+  let jobLocation = document.createElement("li");
+  jobLocation.className = "location";
+  jobLocation.innerHTML = jobData[count].location;
+  ul.appendChild(jobLocation);
+  jobDetails.appendChild(ul);
 
-let divider = document.createElement("div");
-divider.id = "line";
-itemDiv.appendChild(divider);
+  itemDiv.appendChild(jobDetails);
 
-let keyWords = document.createElement("div");
-keyWords.className = "descriptions";
-keyWords.innerHTML = jobData[count].level;
-keyWords.innerHTML = jobData[count].role;
-if (innerCount = jobData[count].languages.length - 1){
-    keyWords.innerHTML = jobData[count].languages[innerCount];
-    innerCount++;
-}
+  let divider = document.createElement("div");
+  divider.id = "line";
+  itemDiv.appendChild(divider);
 
-console.log("language lengths" + jobData[count].languages.length);
-keyWords.innerHTML = jobData["tools"];
-itemDiv.appendChild(keyWords);
+  let keyWords = document.createElement("div");
+  keyWords.className = "descriptions";
+
+  let roleElem = document.createElement("span");
+  let jobRole = jobData[count].role;
+  roleElem.innerHTML = jobRole;
+  keyWords.appendChild(roleElem);
+
+  let levelElem = document.createElement("span");
+  let jobLevel = jobData[count].level;
+  levelElem.innerHTML = jobLevel;
+  keyWords.appendChild(levelElem);
+
+  for (let i = 0; i < jobData[count].languages.length; i++) {
+    let langElem = document.createElement("span");
+    langElem.innerHTML = jobData[count].languages[i];
+    keyWords.appendChild(langElem);
+  }
+
+  for (let j = 0; j < jobData[count].tools.length; j++) {
+    let toolsElem = document.createElement("span");
+    toolsElem.innerHTML = jobData[count].tools[j];
+    keyWords.appendChild(toolsElem);
+  }
+
+  console.log("language lengths" + jobData[count].languages.length);
+  console.log("languages " + jobData[count].languages);
+
+  itemDiv.appendChild(keyWords);
 
   mainDiv.appendChild(itemDiv);
   document.body.appendChild(mainDiv);
 
   count++;
+}
+
+// for (var i = 0; i <= adees.length(); i++) {
+//   x = document.createElement('div').innerHTML = "<label class='bo bp'><input type='checkbox' name='adees[" + adees[i] + "]' value=" + adees[i] + ">Test " + adees[i] + "</label></div>";
+//   document.body.appendChild(x);
+// };
+
+for (let c2 = 0; c2 <= jobData.languages.length(); c2++) {
+  y = document.createElement("span").innerHTML = jobData.languages[c2];
+  document.body.appendChild(y);
 }
 
 // let mainDiv = document.createElement("div");
